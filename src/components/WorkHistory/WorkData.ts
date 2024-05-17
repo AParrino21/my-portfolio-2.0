2b7415b47dea20e8a234c2166f698c72ea1ea8f7
Anthony Parrino <anthonyparrino90@gmail.com>
2024-05-16 23:33:19 -0400

    workdata updates

--- src/components/WorkHistory/WorkData.ts
+++ src/components/WorkHistory/WorkData.ts
@@ -1,16 +1,195 @@
 export const workHistory = [
-    {
-        company: "Infor",
-        timeServed: "2022 - Prestent",
-        everyDayTech: ["React", "TypeScript", "AWS", "Python", "Piral", "Git", "SAM", "Teams", "Node", "yarn", "npm", ],
-        projects: [{name: "Insight", description: "Internal web application with a primary focus of streamlining project creation across multiple team within the company. Allows for simplification of otherwise complicated aws service management."}, {name: "Plugin Manager", description: ""}],
-        achievements: [""]
-    },
-    {
-        company: "",
-        timeServed: "",
-        everyDayTech: [""],
-        projects: [{name: "", description: ""}],
-        achievements: [""]
-    },
-]
+  {
+    company: "Infor",
+    timeServed: "2022 - Prestent",
+    everyDayTech: [
+      "React",
+      "TypeScript",
+      "AWS",
+      "Python",
+      "GraphQL",
+      "Piral",
+      "Git",
+      "Powershell",
+      "SAM",
+      "Teams",
+      "Node",
+      "Yarn",
+      "NPM",
+      "Figma",
+      "Jira",
+    ],
+    projects: [
+      {
+        name: "Insight",
+        description:
+          "An internal web application engineered to streamline project creation across various teams within the company, with a key emphasis on simplifying complex AWS service management processes.",
+      },
+      {
+        name: "Plugin/Permission Manager",
+        description:
+          "Dashboard for managing plugins created on Insight. Allows for control of verison updates, IAM permissions management, rollbacks, archiving,plugin deletion and renaming",
+      },
+      {
+        name: "Teams Announcements",
+        description:
+          "A comprehensive company-wide plugin dashboard designed to centralize critical project announcements and updates posted on Microsoft Teams. With proper permissions, users can seamlessly create posts across one or multiple channels, enhancing communication within the organization.",
+      },
+      {
+        name: "Quicksight Plugin",
+        description:
+          "Built an intricate system of folders, files and dashboards using the team's AWS quicksight service from the ground up. Also built out the abiltiy to search and filter through dashboards, save favorites and view recents. This plugin allows users to seemlessly view all of the team's dashboard metrics for our AWS services that are currently in use.",
+      },
+      {
+        name: "",
+        description: "",
+      },
+    ],
+    achievements: "AWS Ceritfied Developer Associate License",
+    extras: "Currently enrolled in multiple ML/AI courses",
+  },
+  {
+    company: "Senior Coding Tutor",
+    timeServed: "2021 - 2023",
+    everyDayTech: [
+      "React",
+      "JavaScript",
+      "JQuery",
+      "Node",
+      "HTML5",
+      "CSS3",
+      "Express",
+      "OOP",
+      "MySQL",
+      "MongoDB",
+      "Handlebars",
+      "Zoom",
+      "Slack",
+      "Git",
+      "APIs",
+    ],
+    projects: [
+      {
+        name: "Password Generator",
+        description: "Creates random passwords influenced by user prompts.",
+      },
+      { name: "Code Quiz", description: "Multiple choice quiz applications." },
+      {
+        name: "Work Scheduler",
+        description: "Allows user to add tasks to a timeblock.",
+      },
+      {
+        name: "Weather App",
+        description:
+          "Displays current and future weather info based on city search.",
+      },
+      {
+        name: "Employee Tracker",
+        description: "CLI based CMS for a company keeping track of employees.",
+      },
+      { name: "E-commerce", description: "Full Stack ecommerce web app." },
+      {
+        name: "Tech Blog",
+        description:
+          "Full Stack application for blog posts with social-media-like features.",
+      },
+      {
+        name: "Books Search Engine",
+        description:
+          "Full Stack application that allows users to search, save and buy books using the Google Books API.",
+      },
+    ],
+    achievements: "Senior level title",
+    extras:
+      "Helped 100s of students over three years achieve success in their coding journeys",
+  },
+  {
+    company: "AXAD Capital",
+    timeServed: "2021 - 2022",
+    everyDayTech: [
+      "React",
+      "JavaScript",
+      "Node",
+      "HTML5",
+      "CSS3",
+      "Express",
+      "MongoDB",
+      "Skype",
+      "Zoom",
+      "Slack",
+      "Git",
+      "APIs",
+      "Figma",
+    ],
+    projects: [
+      {
+        name: "Quote Nanny",
+        description:
+          "Lead generation web application for helping users find car insurance.",
+      },
+      {
+        name: "Adtelligence",
+        description:
+          "AD serving platform allowing registered users to display advertisements.",
+      },
+      {
+        name: "The AXAD Platform",
+        description:
+          "Platform for diplaying all current data and accounts active and inactive within the company.",
+      },
+      {
+        name: "Anderson Pest Pros",
+        description:
+          "Lead generation web application for helping users find most affordable pest control companies.",
+      },
+      {
+        name: "Insta Loans",
+        description:
+          "Lead generation web application that helps users find the best loans for peronal or business use.",
+      },
+      {
+        name: "Life Insurance Freedom",
+        description:
+          "Lead generation web application that helps users find the best quotes for life insurance.",
+      },
+    ],
+    achievements: "Lead level title",
+    extras:
+      "Rebuilt the engineering team, resulting in significant improvements in click-through rates and lead generation, allowing for substantial revenue increases for the company",
+  },
+  {
+    company: "Fortitude Development",
+    timeServed: "2019 - 2023",
+    everyDayTech: [
+      "React",
+      "Next.JS",
+      "JavaScript",
+      "Node",
+      "HTML5",
+      "CSS3",
+      "Express",
+      "MongoDB",
+      "Zoom",
+      "Slack",
+      "Git",
+      "APIs",
+      "Figma",
+      "Jira",
+    ],
+    projects: [
+      {
+        name: "Facility Finder",
+        description:
+          "An intuitive full stack search application designed specifically for the elderly, enabling them to locate the most cost-effective retirement homes within a specified radius.",
+      },
+      {
+        name: "Facility Finder CMS",
+        description:
+          "Customer Management System for the owners of the Facility Finder full stack web application.",
+      },
+    ],
+    achievements: "Director level title",
+    extras:
+      "Start up company that I was brought onto during a stage of revamping.",
+  },
+];
